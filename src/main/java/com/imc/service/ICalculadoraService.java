6c2635514e962080ea04693fa9b7bbd3bb390982
Jefferson <jefferson.luiz.cruz@gmail.com>
2021-05-25 17:14:08 -0300

    Criação da interface Service

---
+++ src/main/java/com/imc/service/ICalculadoraService.java
@@ -0,0 +1,12 @@
+package com.imc.service;
+
+public interface ICalculadoraService {
+
+	public Double somar(Double valor1, Double valor2);
+
+	public Double subtrair(Double valor1, Double valor2);
+
+	public Double multiplicar(Double valor1, Double valor2);
+
+	public Double dividir(Double valor1, Double valor2);
+}
---
+++ src/main/java/com/imc/service/impl/CalculadoraService.java
@@ -0,0 +1,34 @@
+package com.imc.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.imc.service.ICalculadoraService;
+
+@Service
+public class CalculadoraService implements ICalculadoraService {
+
+	@Override
+	public Double somar(Double valor1, Double valor2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Double subtrair(Double valor1, Double valor2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Double multiplicar(Double valor1, Double valor2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Double dividir(Double valor1, Double valor2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
